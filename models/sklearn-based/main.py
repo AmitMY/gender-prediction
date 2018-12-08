a7fb6225fd37860d0475df0c6b9d50e539a8728a
Eva Vanmassenhove <vanmassenhove.eva@gmail.com>
2018-12-08 12:29:42 +0000

    Fixing merging issues

--- models/sklearn-based/main.py
+++ models/sklearn-based/main.py
@@ -6,7 +6,6 @@ import importlib
 import numpy as np 
 import pickle
 
-from data.reader import Data 
 mod = importlib.import_module("models.sklearn-based.postagger")
 
 from sklearn.feature_extraction.text import CountVectorizer
