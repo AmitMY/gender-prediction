075c2ac36fc452b2026f225789350d434dbcd6f9
Eva Vanmassenhove <vanmassenhove.eva@gmail.com>
2018-12-08 12:37:23 +0000

    Conflict fixes

--- data/reader.py
+++ data/reader.py
@@ -1,24 +1,32 @@
 from functools import lru_cache
-from itertools import chain
 import random
 from utils.file_system import listdir
-from regex import findall
+from re import findall
+
 
 import os
 import spacy
+
 script_dir = os.path.dirname(__file__)
 
 nlp = spacy.load("nl_core_news_sm")
 
 from spacy.lang.nl import Dutch
+
 tokenizer = Dutch().Defaults.create_tokenizer(nlp)
+
 
 @lru_cache(maxsize=None)
 def cached_tokenizer(text):
     return " ".join(map(str, tokenizer(text)))
 
+
 class Data:
-    def __init__(self, name, source, specific=None, tokenize=True):
+    def __init__(self, name, source, specific=None, tokenize=True, clusterF='/home/evanmas2/NMTdata/data/ClinSharedTask/cluster-semantic-vectors/glove_clusters_0.011442950_words.json'):
+        
+        self.clusterF=clusterF
+        self.clusterdata=self.getClusters()
+        
         if isinstance(source, str):
             files = listdir(os.path.join(script_dir, source))
             self.categories = {f.split("_")[1].split(".")[0].lower(): self.parse_file(f, tokenize) for f in files}
@@ -36,8 +44,17 @@ class Data:
 
         data = list(map(lambda m: {"id": m[0], "genre": m[1], "gender": m[2],
                                    "text": m[3] if not tokenize else cached_tokenizer(m[3])}, matches))
-        random.Random(1234).shuffle(data) # Same shuffle seed
+        random.Random(1234).shuffle(data)  # Same shuffle seed
         return data
+
+    def getClusters(self):
+        with open(self.clusterF) as json_file:
+            data = json.load(json_file)
+        data_inv = {}
+        for k, v in data.items():
+            for i in v:
+                data_inv[i] = k
+        return data_inv
 
     def split(self, percent=0.9):
         data1 = Data("train", {w: c[:int(len(c) * percent)] for w, c in self.categories.items()})
@@ -45,8 +62,26 @@ class Data:
 
         return data1, data2
 
-    def export(self):
-        pairs = [(w + ": " + c["text"], 0 if c["gender"] == "M" else 1)
+    def getclusters(self,t,y=None):
+        clusterS = [self.clusterdata[s] if s in self.clusterdata else "0" for s in t.split()]
+        return " ".join(clusterS)
+
+    def export(self, prefix=False, lowercase=False, clusters=True):
+        def preprocess(t, w):
+            print(t)
+            if prefix:
+                t = w + ": " + t
+
+            if lowercase:
+                t = t.lower()
+            
+            if clusters:
+                t = getclusters(t) 
+                print(t)
+                exit()
+            return t
+
+        pairs = [(preprocess(c["text"], w), 0 if c["gender"] == "M" else 1)
                  for w, co in self.categories.items() for c in co]
 
         return list(zip(*pairs))  # List of texts, list of tags
@@ -55,6 +90,5 @@ class Data:
 if __name__ == "__main__":
     data = Data("All", "train", tokenize=True)
     train, dev = data.split()
-
     texts, categories = train.export()
     print("\n", texts[-1], "|", categories[-1])
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from json import load, dump
 from random import shuffle
 
 from data.reader import Data
-from models.spacy.main import main as spacy_main
-from models.pytorch.main import main as pytorch_main
+from models.spacy.main import ModelRunner as spacy_runner
+from models.pytorch.main import ModelRunner as pytorch_runner
+from models.lm_based.main import ModelRunner as kenlm_runner
+from models.sklearn_based.main import ModelRunner as sklearn_runner
+from utils.file_system import makedir, rmfile
+
+import hashlib
 
 data = {
-    "original": Data("All", "train", ["twitter", "youtube", "news"]),
+    # "original": Data("All", "train", ["twitter", "youtube", "news"]),
 
     "twitter": Data("Twitter", "train", ["twitter"]),
     "youtube": Data("Youtube", "train", ["youtube"]),
@@ -19,7 +24,7 @@ data = {
 }
 
 scenarios = {
-    "Original 90%|Original 10%": data["original"].split(),
+    # "Original 90%|Original 10%": data["original"].split(),
     "Twitter 90%|Twitter 10%": data["twitter"].split(),
     "YouTube 90%|YouTube 10%": data["youtube"].split(),
     "News 90%|News 10%": data["news"].split(),
@@ -29,16 +34,26 @@ scenarios = {
 }
 
 models = {
-    "Spacy": (spacy_main, "nl_core_news_sm", {}),
+    "Spacy.n": (spacy_runner, "nl_core_news_sm", {"lowercase": False, "prefix": False}),
+    "Spacy.y": (spacy_runner, "nl_core_news_sm", {"lowercase": True, "prefix": False}),
 }
 
-# Add all of the pytorch models
-for m in ["RNN", "CNN", "RCNN", "LSTM", "LSTMAttention", "SelfAttention"]:
-    # for m in ["CNN", "RCNN", "SelfAttention"]:
-    models[m] = (pytorch_main, m, {})
-    models[m + "+"] = (pytorch_main, m, {"pretrained": "fasttext"})
+#for ngram in range(1, 10):
+#    models["KENLM.n." + str(ngram)] = (kenlm_runner, "KENLM", {"lowercase": False, "ngram": ngram})
+#    models["KENLM.y." + str(ngram)] = (kenlm_runner, "KENLM", {"lowercase": True, "ngram": ngram})
 
-NUM_RUNS = 3
+#for t in ['svm', 'log', 'rf', 'nb', 'knn']:
+#    models["SKLearn-" + t + ".n"] = (sklearn_runner, t, {"lowercase": False})
+#    models["SKLearn-" + t + ".y"] = (sklearn_runner, t, {"lowercase": True})
+
+# Add all of the pytorch models
+#for m in ["RNN", "CNN", "RCNN", "LSTM", "LSTMAttention", "SelfAttention"]:
+#    models[m + ".y"] = (pytorch_runner, m, {"lowercase": True, "prefix": False})
+#    models[m + ".n"] = (pytorch_runner, m, {"lowercase": False, "prefix": False})
+#    models[m + ".y+"] = (pytorch_runner, m, {"lowercase": True, "prefix": False, "pretrained": "fasttext"})
+#    models[m + ".n+"] = (pytorch_runner, m, {"lowercase": False, "prefix": False, "pretrained": "fasttext"})
+
+NUM_RUNS = 2
 models = {w + "." + str(i): c for w, c in models.items() for i in range(NUM_RUNS)}
 
 res_file_name = "results.json"
@@ -52,34 +67,60 @@ except:
 print("Current Results")
 print(results)
 
+checkpoints_dir = "models/checkpoints/"
+makedir(checkpoints_dir)
+
 scenarios_shuffled = list(scenarios.items())
 shuffle(scenarios_shuffled)
+print(scenarios_shuffled[0])
+exit()
 for name, (train, dev) in scenarios_shuffled:
+
+    hashed = hashlib.md5(name.encode('utf-8')).hexdigest()
+    checkpoints_dir_scenario = checkpoints_dir + hashed + "/"
+    makedir(checkpoints_dir_scenario)
+
     if name not in results:
         results[name] = {}
-    for model_name, (runner, model, options) in models.items():
-        print(name, model_name, model, options)
-        if model_name in results[name]:
+
+    models_shuffled = list(models.items())
+    shuffle(models_shuffled)
+    for model_name, (runner, model, options) in models_shuffled:
+        print(name, model_name, model, options, hashed)
+        if model_name in results[name] and results[name][model_name]["best"] > 0:
             print("Skipping", model_name, name, options)
         else:
             all_scores = []
             best_score = 0
             early_stop = 0
-            for score in runner(model=model, train=train, dev=dev, opt=options):
+
+            inst = runner(model=model, train=train, dev=dev, opt=options)
+
+            for score in inst.train():
                 all_scores.append(score)
                 if score > best_score:
                     best_score = score
                     early_stop = 0
+
+                    # Save
+                    print("Saving...")
+                    f_name = checkpoints_dir_scenario + model_name
+                    rmfile(f_name)
+                    inst.save(f_name)
                 else:
                     early_stop += 1
 
-                if early_stop > 5:
+                if early_stop >= 5:
                     break
 
             try:
                 results = load(open(res_file_name, "r"))
             except:
                 pass
+
+            if name not in results:
+                results[name] = {}
+
             results[name][model_name] = {
                 "scores": all_scores,
                 "best": best_score
